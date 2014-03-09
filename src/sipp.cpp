cb0a190ac4a6e8303d79924d3e8eca3a67a7f9cd
Rob Day <rkd@rkd.me.uk>
2014-03-09 18:32:24 +0000

    3.4.1 version number bump

--- src/sipp.cpp
+++ src/sipp.cpp
@@ -1245,7 +1245,7 @@ int main(int argc, char *argv[])
                 }
                 exit(EXIT_OTHER);
             case SIPP_OPTION_VERSION:
-                printf("\n SIPp v3.4.0"
+                printf("\n SIPp v3.4.1"
 #ifdef _USE_OPENSSL
                        "-TLS"
 #endif
