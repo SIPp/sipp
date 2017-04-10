fe83b3f16e1beb418d0f1390da0f21c1b4a86d0e
Nikolay Shopik <shopik@nvcube.net>
2017-04-10 19:06:36 +0300

    Convert to FlatTables

--- docs/conf.py
+++ docs/conf.py
@@ -95,6 +95,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',
+    ],
+}
 
 # -- Options for HTMLHelp output ------------------------------------------
 
