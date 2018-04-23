SIPp/sipp

commit cc9b3408d1d20ccb0638e5ff94106e9644e48879

Probably fix valgrind check from issue 156
Closes #156.

src/sipp.cpp
@@ -2018,6 +2018,7 @@ int main(int argc, char *argv[])
 #endif
 
     free(scenario_file);
+    delete ctrl_socket;
 
     sipp_exit(EXIT_TEST_RES_UNKNOWN);
 }

.travis.yml
@@ -20,6 +20,7 @@ addons:
       - libncurses5-dev
       - libssl-dev
       - libgsl0-dev
+      - valgrind
 
 env:
   - CONFOPTS="--with-gsl --with-openssl --with-pcap --with-rtpstream --with-sctp"

regress/github-#0156/run
@@ -3,8 +3,6 @@
 # Author: Walter Doekes, OSSO B.V., 2015
 . "`dirname "$0"`/../functions"; init
 
-skip "will fix later"
-
 # Valgrind on UAS.
 valgrind --xml=yes --xml-file=uas.log --partial-loads-ok=yes \
     --show-leak-kinds=all --leak-check=full `get_sipp` -nostdin -m 10 -sn uas \

commit f2cf583bd237467c33be7bb892d46c8c6c4adf79

Fix double-free issue introduced in cc9b3408

src/sipp.cpp
@@ -1991,12 +1991,15 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (traffic_thread()) {
-        if (!nostdin) {
-            stdin_socket->close();
-        }
-        ctrl_socket->close();
+    traffic_thread();
+
+    /* Clean up */
+    if (!nostdin) {
+        stdin_socket->close();
+        stdin_socket = NULL; /* close "delete's this" */
     }
+    ctrl_socket->close();
+    ctrl_socket = NULL;  /* close "delete's this" */
 
     /* Cancel and join other threads. */
     if (pthread2_id) {
@@ -2018,7 +2021,6 @@ int main(int argc, char *argv[])
 #endif
 
     free(scenario_file);
-    delete ctrl_socket;
 
     sipp_exit(EXIT_TEST_RES_UNKNOWN);
 }