SIPp/sipp

commit f2cf583bd237467c33be7bb892d46c8c6c4adf79

Fix double-free issue introduced in cc9b3408

src/sipp.cpp
@@ -1991,12 +1991,15 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (traffic_thread()) {
-        if (!nostdin) {
-            stdin_socket->close();
-        }
-        ctrl_socket->close();
+    traffic_thread();
+
+    /* Clean up */
+    if (!nostdin) {
+        stdin_socket->close();
+        stdin_socket = NULL; /* close "delete's this" */
     }
+    ctrl_socket->close();
+    ctrl_socket = NULL;  /* close "delete's this" */
 
     /* Cancel and join other threads. */
     if (pthread2_id) {
@@ -2018,7 +2021,6 @@ int main(int argc, char *argv[])
 #endif
 
     free(scenario_file);
-    delete ctrl_socket;
 
     sipp_exit(EXIT_TEST_RES_UNKNOWN);
 }

commit 6a818a5570577d88d9642bcf434e5ff6b6c06a8d

Fix traffic thread crash
Crash was introduced by commit f2cf583bd237467c33be7bb892d46c8c6c4adf79 when an if w/curly braces surrounding the call to traffic_thread() was forgotten.

src/sipp.cpp
@@ -1991,15 +1991,15 @@ int main(int argc, char *argv[])
         }
     }
 
-    traffic_thread();
-
-    /* Clean up */
-    if (!nostdin) {
-        stdin_socket->close();
-        stdin_socket = NULL; /* close "delete's this" */
+    if (traffic_thread()) {
+        /* Clean up */
+        if (!nostdin) {
+            stdin_socket->close();
+            stdin_socket = NULL; /* close "delete's this" */
+        }
+        ctrl_socket->close();
+        ctrl_socket = NULL;  /* close "delete's this" */
     }
-    ctrl_socket->close();
-    ctrl_socket = NULL;  /* close "delete's this" */
 
     /* Cancel and join other threads. */
     if (pthread2_id) {