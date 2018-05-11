SIPp/sipp

commit f2cf583bd237467c33be7bb892d46c8c6c4adf79

Fix double-free issue introduced in cc9b3408

src/sipp.cpp
@@ -1991,12 +1991,15 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (traffic_thread()) {
-        if (!nostdin) {
-            stdin_socket->close();
-        }
-        ctrl_socket->close();
+    traffic_thread();
+
+    /* Clean up */
+    if (!nostdin) {
+        stdin_socket->close();
+        stdin_socket = NULL; /* close "delete's this" */
     }
+    ctrl_socket->close();
+    ctrl_socket = NULL;  /* close "delete's this" */
 
     /* Cancel and join other threads. */
     if (pthread2_id) {
@@ -2018,7 +2021,6 @@ int main(int argc, char *argv[])
 #endif
 
     free(scenario_file);
-    delete ctrl_socket;
 
     sipp_exit(EXIT_TEST_RES_UNKNOWN);
 }

commit 773969cec3a638d8a43eb2d4757b98a1d70fcf67

Fix traffic thread crash upon shutdown (introduced by f2cf583b)
Commit f2cf583b intended to remove a double free upon shutdown but ended up causing a crash when the return value of "traffic_thread()" is false (e.g. shutdown) - this fixes the issue by restoring the original behavior while also using recent changes.

src/sipp.cpp
@@ -1991,15 +1991,15 @@ int main(int argc, char *argv[])
         }
     }
 
-    traffic_thread();
-
-    /* Clean up */
-    if (!nostdin) {
-        stdin_socket->close();
-        stdin_socket = NULL; /* close "delete's this" */
+    if (traffic_thread()) {
+        /* Clean up */
+        if (!nostdin) {
+            stdin_socket->close();
+            stdin_socket = NULL; /* close "delete's this" */
+        }
+        ctrl_socket->close();
+        ctrl_socket = NULL;  /* close "delete's this" */
     }
-    ctrl_socket->close();
-    ctrl_socket = NULL;  /* close "delete's this" */
 
     /* Cancel and join other threads. */
     if (pthread2_id) {
@@ -2021,6 +2021,10 @@ int main(int argc, char *argv[])
 #endif
 
     free(scenario_file);
+    if (ctrl_socket != NULL) {
+        ctrl_socket->close();
+        ctrl_socket = NULL; /* close "delete's this" */
+    }
 
     sipp_exit(EXIT_TEST_RES_UNKNOWN);
 }