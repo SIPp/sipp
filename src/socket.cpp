SIPp/sipp

commit 337f5c0cd6559753af3b7433887fdc415b91b072

Explicitly bind tcp sockets to source port
Quick workaround to get binding working. Also set SO_REUSEADDR for good
measure.

Fixes #170.

src/socket.cpp
@@ -2722,6 +2722,25 @@ int open_connections()
         }
         sipp_customize_socket(tcp_multiplex);
 
+        if (user_port) {
+            int sock_opt = 1;
+
+            if (setsockopt(tcp_multiplex->ss_fd, SOL_SOCKET, SO_REUSEADDR, (void *)&sock_opt,
+                           sizeof (sock_opt)) == -1) {
+                ERROR_NO("setsockopt(SO_REUSEADDR) failed");
+            }
+
+            if (tcp_multiplex->ss_ipv6) {
+                struct sockaddr_in6 sa_loc = {AF_INET6};
+                sa_loc.sin6_port = htons(local_port);
+                sipp_bind_socket(tcp_multiplex, (struct sockaddr_storage*)&sa_loc, NULL);
+            } else {
+                struct sockaddr_in sa_loc = {AF_INET};
+                sa_loc.sin_port = htons(local_port);
+                sipp_bind_socket(tcp_multiplex, (struct sockaddr_storage*)&sa_loc, NULL);
+            }
+        }
+
         if (sipp_connect_socket(tcp_multiplex, &remote_sockaddr)) {
             if (reset_number > 0) {
                 WARNING("Failed to reconnect\n");

commit 79478a7b0bbf9d065a6e7822ffe4260c864e7b80

Fix TCP binding regression (introduced by 337f5c0c)
Commit 337f5c0c caused TCP binding to no longer work on a given
interface (specified by the "-i w.x.y.z" parameter) when
the "-p <port>" commandline parameter is present.

src/socket.cpp
@@ -2568,16 +2568,6 @@ int open_connections()
                            sizeof (sock_opt)) == -1) {
                 ERROR_NO("setsockopt(SO_REUSEADDR) failed");
             }
-
-            if (tcp_multiplex->ss_ipv6) {
-                struct sockaddr_in6 sa_loc = {AF_INET6};
-                sa_loc.sin6_port = htons(local_port);
-                sipp_bind_socket(tcp_multiplex, (struct sockaddr_storage*)&sa_loc, NULL);
-            } else {
-                struct sockaddr_in sa_loc = {AF_INET};
-                sa_loc.sin_port = htons(local_port);
-                sipp_bind_socket(tcp_multiplex, (struct sockaddr_storage*)&sa_loc, NULL);
-            }
         }
 
         if (tcp_multiplex->connect(&remote_sockaddr)) {