776223dc712f8726fc4bd04e4964186aee2eced2
Walter Doekes <walter+github@wjd.nu>
2018-07-19 09:37:09 +0200

    Split off autogen part of build.sh into autogen.sh, improve build.sh
    Adds some more explanations to build.sh usage. Uses nproc cores instead of
    hardcoded 3. Fixes so unittests are run again.

--- build.sh
+++ build.sh
@@ -3,15 +3,23 @@ set -e  # abort on error
 
 MAKE=`which gmake make 2>/dev/null | head -n1`  # prefer GNU make
 test -z "$MAKE" && echo "No (g)make found" >&2 && exit 1
+CPUCOUNT=$(nproc --all 2>/dev/null || echo 1)
+MAKEFLAGS="-j$CPUCOUNT"
 
-# Optional: for git checkout only.
-if which git >/dev/null && test -d .git; then
-	git submodule update --init
+if test -z "$*"; then
+    echo "build.sh: Please specify configure options," \
+        "--none for defaults, or --full for all" >&2
+    exit 1
+elif test "$*" = "--help" || test "$*" = "-h"; then
+    ./configure --help
+    exit 1
 fi
 
-autoreconf -vifs
+./autogen.sh
 
-if test "$*" = "--full"; then
+if test "$*" = "--none"; then
+    ./configure
+elif test "$*" = "--full"; then
     ./configure \
         --with-gsl \
         --with-openssl \
@@ -22,10 +30,10 @@ else
     ./configure "$@"
 fi
 
-# Optional: for git checkout only.
-if test -f gtest/gtest.h; then
-	"$MAKE" -j3 sipp_unittest
+# For git checkout, run unit tests.
+if test -e gtest/.git; then
+	"$MAKE" $MAKEFLAGS sipp_unittest
 	./sipp_unittest
 fi
 
-"$MAKE" -j3
+"$MAKE" $MAKEFLAGS
