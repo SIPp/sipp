58d74ec498c29ae31fc239f1b541bff2cac63fcb
not_today <rajeshkumarsingh381@gmail.com>
2022-12-01 17:52:24 +0530

    print OpenSSL error reason when certificate load fails (#596)

--- src/sslsocket.cpp
+++ src/sslsocket.cpp
@@ -336,14 +336,18 @@ enum tls_init_status TLS_init_context(void)
     if (SSL_CTX_use_certificate_file(sip_trp_ssl_ctx,
                                      tls_cert_name,
                                      SSL_FILETYPE_PEM) != 1) {
-        ERROR("TLS_init_context: SSL_CTX_use_certificate_file failed");
+        char errbuf[256] = {'\0'};
+        ERR_error_string_n(ERR_get_error(), errbuf, sizeof(errbuf));
+        ERROR("TLS_init_context: SSL_CTX_use_certificate_file failed: %s", errbuf);
         return TLS_INIT_ERROR;
     }
 
     if (SSL_CTX_use_certificate_file(sip_trp_ssl_ctx_client,
                                      tls_cert_name,
                                      SSL_FILETYPE_PEM) != 1) {
-        ERROR("TLS_init_context: SSL_CTX_use_certificate_file (client) failed");
+        char errbuf[256] = {'\0'};
+        ERR_error_string_n(ERR_get_error(), errbuf, sizeof(errbuf));
+        ERROR("TLS_init_context: SSL_CTX_use_certificate_file (client) failed: %s", errbuf);
         return TLS_INIT_ERROR;
     }
     if (SSL_CTX_use_PrivateKey_file(sip_trp_ssl_ctx,
