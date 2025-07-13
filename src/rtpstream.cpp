ed402ed9a58f0fee3b602fea1b3cd16596ed8aae
Jeannot Langlois <jeannot.langlois@gmail.com>
2025-07-13 17:04:12 -0400

    Fix RTPCHECK functionality regressions

--- src/rtpstream.cpp
+++ src/rtpstream.cpp
@@ -32,6 +32,7 @@
 #include <memory>
 #include <vector>
 #include <errno.h>
+#include <sstream>
 
 /* stub to add extra debugging/logging... */
 static void debugprint(const char* format, ...)
@@ -126,8 +127,6 @@ int           num_ready_threads = 0;
 int           busy_threads_max = 0;
 int           ready_threads_max = 0;
 
-unsigned int  global_ssrc_id = 0;
-
 FILE*         debugafile = nullptr;
 FILE*         debugvfile = nullptr;
 pthread_mutex_t  debugamutex = PTHREAD_MUTEX_INITIALIZER;
@@ -160,14 +159,14 @@ pthread_cond_t quit_cvaudio = PTHREAD_COND_INITIALIZER;
 pthread_cond_t quit_cvvideo = PTHREAD_COND_INITIALIZER;
 
 // JLSRTP contexts
-JLSRTP g_txUACAudio;
-JLSRTP g_rxUACAudio;
-JLSRTP g_txUACVideo;
-JLSRTP g_rxUACVideo;
-JLSRTP g_rxUASAudio;
-JLSRTP g_txUASAudio;
-JLSRTP g_rxUASVideo;
-JLSRTP g_txUASVideo;
+JLSRTP g_txUACAudio(global_ssrc_id, "127.0.0.1", 0);
+JLSRTP g_rxUACAudio(global_ssrc_id, "127.0.0.1", 0);
+JLSRTP g_txUACVideo(global_ssrc_id, "127.0.0.1", 0);
+JLSRTP g_rxUACVideo(global_ssrc_id, "127.0.0.1", 0);
+JLSRTP g_rxUASAudio(global_ssrc_id, "127.0.0.1", 0);
+JLSRTP g_txUASAudio(global_ssrc_id, "127.0.0.1", 0);
+JLSRTP g_rxUASVideo(global_ssrc_id, "127.0.0.1", 0);
+JLSRTP g_txUASVideo(global_ssrc_id, "127.0.0.1", 0);
 pthread_mutex_t uacAudioMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t uacVideoMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t uasAudioMutex = PTHREAD_MUTEX_INITIALIZER;
@@ -1603,11 +1602,6 @@ int rtpstream_new_call(rtpstream_callinfo_t* callinfo)
     taskinfo->audio_srtp_echo_active = 0;
     taskinfo->video_srtp_echo_active = 0;
 #endif // USE_TLS
-
-    /* generate random ssrc */
-    if (global_ssrc_id == 0) {
-        global_ssrc_id = rand();
-    }
 
     /* rtp stream members */
     taskinfo->audio_ssrc_id = global_ssrc_id++;
@@ -3142,6 +3136,16 @@ int rtpstream_rtpecho_startaudio(rtpstream_callinfo_t* callinfo, JLSRTP& rxUASAu
     debugprint("rtpstream_rtpecho_startaudio callinfo=%p\n", callinfo);
 
     taskentry_t   *taskinfo = callinfo->taskinfo;
+    ParamPass p;
+    std::ostringstream oss;
+    int numFd = -1;
+
+    oss.str("");
+    oss << "debugrefileaudio";
+    oss << "_";
+    oss << time(NULL);
+    oss << ".";
+    oss << "log";
 
     if (!taskinfo)
     {
@@ -3149,8 +3153,6 @@ int rtpstream_rtpecho_startaudio(rtpstream_callinfo_t* callinfo, JLSRTP& rxUASAu
     }
 
 #ifdef USE_TLS
-    ParamPass p;
-
     taskinfo->audio_srtp_echo_active = 1;
 
     pthread_mutex_lock(&debugremutexaudio);
@@ -3158,12 +3160,20 @@ int rtpstream_rtpecho_startaudio(rtpstream_callinfo_t* callinfo, JLSRTP& rxUASAu
     {
         if (debugrefileaudio == nullptr)
         {
-            debugrefileaudio = fopen("debugrefileaudio", "w");
-            if (debugrefileaudio == nullptr)
+            numFd = open(oss.str().c_str(), O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
+            if (numFd >= 0)
             {
-                /* error encountered opening audio debug file */
-                pthread_mutex_lock(&debugremutexaudio);
-                return -2;
+                debugrefileaudio = fdopen(numFd, "w");
+                if (debugrefileaudio == nullptr)
+                {
+                    /* error encountered opening audio debug file */
+                    pthread_mutex_unlock(&debugremutexaudio);
+                    return -2;
+                }
+            }
+            else
+            {
+                return -3;
             }
         }
     }
@@ -3192,6 +3202,9 @@ int rtpstream_rtpecho_startaudio(rtpstream_callinfo_t* callinfo, JLSRTP& rxUASAu
             return -7;
         }
     }
+#else
+    (void)p; // silence compiler warning claiming that p is unused
+    (void)numFd; // silence compiler warning claiming that numFd is unused
 #endif // USE_TLS
 
     return 0;
@@ -3315,6 +3328,16 @@ int rtpstream_rtpecho_startvideo(rtpstream_callinfo_t* callinfo, JLSRTP& rxUASVi
     debugprint("rtpstream_rtpecho_startvideo callinfo=%p\n", callinfo);
 
     taskentry_t   *taskinfo = callinfo->taskinfo;
+    ParamPass p;
+    std::ostringstream oss;
+    int numFd = -1;
+
+    oss.str("");
+    oss << "debugrefilevideo";
+    oss << "_";
+    oss << time(NULL);
+    oss << ".";
+    oss << "log";
 
     if (!taskinfo)
     {
@@ -3322,8 +3345,6 @@ int rtpstream_rtpecho_startvideo(rtpstream_callinfo_t* callinfo, JLSRTP& rxUASVi
     }
 
 #ifdef USE_TLS
-    ParamPass p;
-
     taskinfo->video_srtp_echo_active = 1;
 
     pthread_mutex_lock(&debugremutexvideo);
@@ -3331,12 +3352,20 @@ int rtpstream_rtpecho_startvideo(rtpstream_callinfo_t* callinfo, JLSRTP& rxUASVi
     {
         if (debugrefilevideo == nullptr)
         {
-            debugrefilevideo = fopen("debugrefilevideo", "w");
-            if (debugrefilevideo == nullptr)
+            numFd = open(oss.str().c_str(), O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
+            if (numFd >= 0)
             {
-                /* error encountered opening audio debug file */
-                pthread_mutex_unlock(&debugremutexvideo);
-                return -2;
+                debugrefilevideo = fdopen(numFd, "w");
+                if (debugrefilevideo == nullptr)
+                {
+                    /* error encountered opening video debug file */
+                    pthread_mutex_unlock(&debugremutexvideo);
+                    return -2;
+                }
+            }
+            else
+            {
+                return -3;
             }
         }
     }
@@ -3365,6 +3394,9 @@ int rtpstream_rtpecho_startvideo(rtpstream_callinfo_t* callinfo, JLSRTP& rxUASVi
             return -8;
         }
     }
+#else
+    (void)p; // silence compiler warning claiming that p is unused
+    (void)numFd; // silence compiler warning claiming that numFd is unused
 #endif // USE_TLS
 
     return 0;
